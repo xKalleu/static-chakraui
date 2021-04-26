1341550549f19c0805eca66dc6f5f359a0192208
Kalleu Oliveira <xkalleu@gmail.com>
2021-04-25 21:01:55 -0300

    add: create element props section travel

--- src/components/Travel/index.tsx
+++ src/components/Travel/index.tsx
@@ -1,5 +1,7 @@
 import { Flex, Text, Grid, Image } from '@chakra-ui/react';
 
+import { Icons } from './Icons';
+
 export function Travel() {
 	return (
 		<Flex
@@ -11,40 +13,11 @@ export function Travel() {
 			justify="center"
 			maxWidth={1240}>
 			<Grid w="100%" templateColumns="repeat(5, 1fr)" gap={6}>
-				<Flex w="100%" align="center" justify="center" direction="column">
-					<Image src="./images/cocktail.png" alt="Airplane" />
-					<Text fontWeight="600" color="gray.700" fontSize="24px" mt="20px">
-						vida noturna
-					</Text>
-				</Flex>
-
-				<Flex w="100%" align="center" justify="center" direction="column">
-					<Image src="./images/surf.png" alt="Airplane" />
-					<Text fontWeight="600" color="gray.700" fontSize="24px" mt="20px">
-						praia
-					</Text>
-				</Flex>
-
-				<Flex w="100%" align="center" justify="center" direction="column">
-					<Image src="./images/building.png" alt="Airplane" />
-					<Text fontWeight="600" color="gray.700" fontSize="24px" mt="20px">
-						moderno
-					</Text>
-				</Flex>
-
-				<Flex w="100%" align="center" justify="center" direction="column">
-					<Image src="./images/museum.png" alt="Airplane" />
-					<Text fontWeight="600" color="gray.700" fontSize="24px" mt="20px">
-						clássico
-					</Text>
-				</Flex>
-
-				<Flex w="100%" align="center" justify="center" direction="column">
-					<Image src="./images/earth.png" alt="Airplane" />
-					<Text fontWeight="600" color="gray.700" fontSize="24px" mt="20px">
-						e mais...
-					</Text>
-				</Flex>
+				<Icons src="./images/cocktail.png">vida noturna</Icons>
+				<Icons src="./images/surf.png">praia</Icons>
+				<Icons src="./images/building.png">moderno</Icons>
+				<Icons src="./images/museum.png">clássico</Icons>
+				<Icons src="./images/earth.png">e mais...</Icons>
 			</Grid>
 		</Flex>
 	);
