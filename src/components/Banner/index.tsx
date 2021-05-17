f1e344495c5a240be539d95e16e12028ebf9cd43
Kalleu Oliveira <xkalleu@gmail.com>
2021-05-17 20:35:41 -0300

    add: ajust mobile

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -12,19 +12,27 @@ export function Banner() {
 			bgRepeat="no-repeat"
 			bgSize="cover"
 			bgImage="url('/images/background.png')"
-			minHeight="335">
-			<Grid w="100%" maxWidth={1240} templateColumns="repeat(2, 1fr)" gap={6}>
-				<Flex w="100%" justify="center" direction="column">
-					<Text fontWeight="400" color="gray.50" fontSize="4xl">
+			minHeight={['163', '335']}>
+			<Grid
+				w="100%"
+				maxWidth={1240}
+				templateColumns={['1fr', 'repeat(2, 1fr)']}
+				gap={6}>
+				<Flex w="100%" justify="center" ml={['16px', '0']} direction="column">
+					<Text fontWeight="400" color="gray.50" fontSize={['20px', '4xl']}>
 						5 Continentes,
 						<br /> infinitas possibilidades.
 					</Text>
-					<Text fontWeight="400" color="gray.100" fontSize="xl" mt="20px">
-						Chegou a hora de tirar do papel a viagem que você
-						<br /> sempre sonhou.
+					<Text
+						fontWeight="400"
+						color="gray.100"
+						mr={['16px', '0']}
+						fontSize={['14px', 'xl']}
+						mt="20px">
+						Chegou a hora de tirar do papel a viagem que você sempre sonhou.
 					</Text>
 				</Flex>
-				<Flex w="100%" align="center" justify="center">
+				<Flex w="100%" align="center" d={['none', 'flex']} justify="center">
 					<Image
 						pos="relative"
 						top="50px"
